68cb23391b0711cdbbdd875ca19f8063671373d0
I-Chanwoo <chanuel0110@gmail.com>
2024-01-23 09:38:41 +0900

    이찬우/ 유클리드 호제법을 통한 최대공약수

---
+++ baekjoon_study/common_divisor_multiple.py
@@ -0,0 +1,28 @@
+'''
+유클리드 호제법
+ 1.두 수 중 더 작은 수로 나눔
+ 2. 나머지가 0이 아니라면 작은수를 나머지로 나눔 
+ 3. 나머지가 0이 될 때까지 1, 2 반복
+ 4, 나머지가 0이 되었을 때의 몫이 최대공
+'''
+
+def common_divisior(input1, input2):
+    max_input = max(input1,input2)
+    min_input = min(input1, input2)
+    
+    remain = max_input%min_input
+    greatest  = max_input/min_input
+
+    while(True):
+        if (remain == 0):
+            break
+        max_input = min_input
+        min_input = remain
+        greatest  = max_input/min_input
+        remain = max_input%min_input
+    return int(greatest)
+
+x1 = int(input("정수를 입력하세요:"))
+x2 = int(input("정수를 입력하세요:"))
+print(common_divisior(x1,x2))
+
